d6d979b0266f5b55c828fe1c2ccd2bffde99bef5
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 06:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function BoardMember (name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = function(){return "No, I must disagree"}
+  this.approve = function(){return "You can do that!"}
+  this.veto = function(){return "No, I must disagree"}
+  this.veto = function(){return "No, I must disagree"}
 }
